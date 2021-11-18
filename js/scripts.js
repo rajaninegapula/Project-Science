c738d97abe7ff41891c6d47ac9cd09306aa93553
Rajani Negapula <91852114+rajaninegapula@users.noreply.github.com>
2021-11-18 16:56:28 +0100

    Create scripts.js
    sample alert created

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+alert('Hello world');
+console.log('Hello world');
+favoritefood = 'fruits';
+document.write(favoritefood);
+
+/* Just practicing
+let myName = 'Bob';
+document.write(myName);
+myname = 'John Doe';
+document.write(myname);
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size/2);
+document.write(minSize);*/
