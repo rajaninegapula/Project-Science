3f9398550177e63866d0071775503f80b0ac9e33
Rajani Negapula <91852114+rajaninegapula@users.noreply.github.com>
2021-11-20 11:23:48 +0100

    Update scripts.js
    Pokemon repository created with simple pokemon object list in an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
-alert('Hello world');
-console.log('Hello world');
-favoritefood = 'fruits';
-document.write(favoritefood);
-
-/* Just practicing
-let myName = 'Bob';
-document.write(myName);
-myname = 'John Doe';
-document.write(myname);
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size/2);
-document.write(minSize);*/
+//pokemon list in an array
+let pokemonlist = [{
+    name: 'Squirtle',
+    height: 1.08,
+    type: ['water']
+  },
+  {
+    name: 'Bulbasaur',
+    height: 2.04,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 2.0,
+    type: ['fire']
+  },
+  {
+    name: 'Butterfree',
+    height: 3.07,
+    type: ['bug', 'flying']
+  }
+];
