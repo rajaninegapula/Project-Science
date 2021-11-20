6523316c3889d63600a39ae8473542d1f696369f
Rajani Negapula <91852114+rajaninegapula@users.noreply.github.com>
2021-11-20 19:12:50 +0100

    Update scripts.js
    Corrected the 'type' of objects to 'types'

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,21 @@
 let pokemonList = [{
     name: 'Squirtle',
     height: 1.08,
-    type: ['water']
+    types: ['water']
   },
   {
     name: 'Bulbasaur',
     height: 2.04,
-    type: ['grass', 'poison']
+    types: ['grass', 'poison']
   },
   {
     name: 'Charmander',
     height: 2.0,
-    type: ['fire']
+    types: ['fire']
   },
   {
     name: 'Butterfree',
     height: 3.07,
-    type: ['bug', 'flying']
+    types: ['bug', 'flying']
   }
 ];
