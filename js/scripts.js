1af61d9ffa22e7c426509d2fde55e3bdf0ee6346
Rajani Negapula <91852114+rajaninegapula@users.noreply.github.com>
2021-11-30 18:27:15 +0100

    Update scripts.js
    assignment 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,89 @@
-//pokemon list in an array
-let pokemonList = [{
-    name: 'Squirtle',
-    height: 1.08,
-    types: ['water']
-  },
-  {
-    name: 'Bulbasaur',
-    height: 2.04,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 2.0,
-    types: ['fire']
-  },
-  {
-    name: 'Butterfree',
-    height: 3.07,
-    types: ['bug', 'flying']
-  }
+let pokemonList=[
+
+    {
+        name: 'Charmander',
+        height:0.6,
+        type:["Monster", "Dragon"]
+    },
+
+    {
+       name: 'Bulbasaur',
+        height:0.7,
+        type:["Monster", "Grass"]
+    },
+
+    {
+        name: 'Blastoise',
+        height:0.6,
+        type:["Monster", "Water 1"]
+    },
+
+    {
+        name: 'Ivysaur',
+        height:1,
+        type:["Monster", "Grass"]
+    },
+
+    {
+        name: 'Venusaur',
+        height:2,
+        type:["Monster", "Grass"]
+    },
+
+    {
+        name: 'Charizard',
+        height:1.7,
+        type:["Monster", "Dragon"]
+    },
+
+     {
+        name: 'Nidoking',
+        height:1.4,
+        type:["Monster", "Field"]
+    },
+
+     {
+        name: 'Nidorino',
+        height:0.9,
+        type:["Monster", "Field"]
+    },
+
+     {
+        name: 'Charmeleon',
+        height:1.1,
+        type:["Monster", "Dragon"]
+    },
+
+
+
 ];
-//creating a 'for' loop and 'if else' condition for checking top height pokemon object
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 3) {
-    document.write("<ul><li>" + pokemonList[i].name  + " height: " + pokemonList[i].height +  " Wow, that’ s biggg! " + "</li></ul>");
-    // console.log(pokemonList[i].name + "Wow, that’ s biggg!");
-  } else {
-    document.write("<ul><li>" + pokemonList[i].name + " height: " + pokemonList[i].height + "</li></ul>");
-  }
-}
+
+// IIFE pokemonRepository
+let pokemonRepository = (function () {
+   return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
+
+  pokemonRepository.add({ name: 'Ferrothorn', height: 1, types:['grass', 'mineral'] });
+  pokemonRepository.getAll();
+
+  // IIFE function to loop through the  pokemonList array
+(function () {
+    pokemonList.forEach(function(pokemon) {
+        let pokemonName = pokemon.name
+        let pokemonHeight = pokemon.height
+        /* This checks to see if a pokemon height is greater than or equal to 2 the add wow, that's big. Otherwise it should just print theri names and hight */
+        if(pokemonHeight >= 2) {
+            document.write("<p>" + pokemonName + ' (height : ' + pokemonHeight + ')' + ' - Wow, that\'s big!' + "</p>");
+        } else {
+            document.write("<p>" + pokemonName + ' (height : ' + pokemonHeight + ')' + "</p>");
+        }
+
+        });
+})();
